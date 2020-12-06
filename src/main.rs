77f3b08dbfbeb05c4a801bc095908cbe5779631d
Jacob Jonsson <jacob.jonsson@schibsted.com>
2020-12-06 12:06:22 +0100

    Load using pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[path = "./day_1/day_1.rs"]
 mod day_1;
 
@@ -17,9 +19,17 @@ mod day_5;
 mod day_6;
 
 fn main() {
-    day_1::day1();
-    day_2::day2();
-    day_4::day4();
-    day_5::day5();
-    day_6::day6();
+    let args: Vec<String> = env::args().collect();
+
+    let day = &args[1];
+
+    match day.as_str() {
+        "1" => day_1::day1(),
+        "2" => day_2::day2(),
+        "3" => day_3::day3(),
+        "4" => day_4::day4(),
+        "5" => day_5::day5(),
+        "6" => day_6::day6(),
+        _ => panic!("Provided day does not exist"),
+    };
 }
